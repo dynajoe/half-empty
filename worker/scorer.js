e4b9da269515eb2486a9a62884b22e96242d35f7
Scott Smerchek <scott.smerchek@gmail.com>
2013-06-15 11:54:53 -0500

    Extracted fetching of tweets to it's own file.

--- worker/scorer.js
+++ worker/scorer.js
@@ -1,16 +1,8 @@
 var ironio = require('node-ironio')('nfFVh41-R6ZkFU0SzGOgzJM9JCk')
   , project = ironio.projects('51bbd144ed3d766cf3000ab6')
   , cache = project.caches('twitter')
-  , request = require('request');
-
-var twitter = require('ntwitter');
-
-var twit = new twitter({
-  consumer_key: '6F9g1bQtl8l14AkJzBgw',
-  consumer_secret: 'ix9O34lNgaVqIk6TJvKp5AZbeboMDF8MxBEIl8WtIQ',
-  access_token_key: '49359644-yJBynxQGA5eYEpAwcOq8RzWP9X0gUIOnNPOaEBeH4',
-  access_token_secret: 'etIdekP6F3vAse8iD3K5nxKSQHyJZN8xiCbGLIIls'
-});
+  , request = require('request')
+  , tweets = require('./lib/tweets');
 
 cache.info(function(err, res) {
    console.log('Conneted to cache: ' + JSON.stringify(res));
@@ -23,11 +15,8 @@ require('./lib/payload_parser').parse_payload(process.argv, function (payload) {
       console.error('No twitter handle defined.');
       process.exit(1);
    }
-   twit.getUserTimeline({
-      screen_name: payload.handle,
-      count: 5
-   }, function(err, data) {
-      cache.put(payload.handle, JSON.stringify(data), function(err, msg) {
+   tweets.getTweets(payload.handle, function(err, tweets) {
+      cache.put(payload.handle, JSON.stringify(tweets), function(err, msg) {
          if(err) {
             console.error('Failed to put to cache. ', err);
             process.exit(1);
