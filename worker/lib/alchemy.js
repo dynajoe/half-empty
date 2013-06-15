954455ef50504ccfc91947d302dc8b3381b40249
Scott Smerchek <scott.smerchek@gmail.com>
2013-06-15 12:29:40 -0500

    Added sentiment tagging for each tweet retrieved. Sentiment is stored on the tweet object.

---
+++ worker/lib/alchemy.js
@@ -0,0 +1,24 @@
+var request = require('request')
+  , async = require('async');
+
+module.exports.analyzeTweets = function(tweets, cb) {
+   async.map(tweets, analyzeTweet, function(err, results) {
+      if (err) return cb(err);
+      return cb(null, results);
+   });
+}
+
+function analyzeTweet(tweet, cb) {
+   request.post('http://access.alchemyapi.com/calls/text/TextGetTextSentiment', {
+      form: {  
+         apikey: '435de1c638f3743fec343bc8e7df8e088c0581e7', 
+         outputMode: 'json',
+         text: tweet.text
+      },
+      json: true
+   }, function(err, res, data) {
+      if (err) return cb(err);
+      tweet.sentiment = data.docSentiment;
+      return cb(null, tweet);
+   });
+}
--- worker/scorer.js
+++ worker/scorer.js
@@ -2,7 +2,8 @@ var ironio = require('node-ironio')('nfFVh41-R6ZkFU0SzGOgzJM9JCk')
   , project = ironio.projects('51bbd144ed3d766cf3000ab6')
   , cache = project.caches('twitter')
   , request = require('request')
-  , tweets = require('./lib/tweets');
+  , twitter = require('./lib/twitter')
+  , alchemy = require('./lib/alchemy');
 
 cache.info(function(err, res) {
    console.log('Conneted to cache: ' + JSON.stringify(res));
@@ -15,13 +16,23 @@ require('./lib/payload_parser').parse_payload(process.argv, function (payload) {
       console.error('No twitter handle defined.');
       process.exit(1);
    }
-   tweets.getTweets(payload.handle, function(err, tweets) {
-      cache.put(payload.handle, JSON.stringify(tweets), function(err, msg) {
+   twitter.getTweets(payload.handle, function(err, tweets) {
+      if(err) {
+         console.error('Failed to retrieve tweets for user: ' + payload.handle, err);
+         process.exit(1);
+      }
+      alchemy.analyzeTweets(tweets, function(err, analyzedTweets) {
          if(err) {
-            console.error('Failed to put to cache. ', err);
+            console.error('Failed to analyze tweets for user: ' + payload.handle, err);
             process.exit(1);
          }
-         console.log('Successfully stored ' + payload.handle + '\'s tweets! ' + JSON.stringify(msg));
+         cache.put(payload.handle, JSON.stringify(tweets), function(err, msg) {
+            if(err) {
+               console.error('Failed to put to cache. ', err);
+               process.exit(1);
+            }
+            console.log('Successfully stored ' + payload.handle + '\'s tweets! ' + JSON.stringify(msg));
+         });
       });
    });
 });
