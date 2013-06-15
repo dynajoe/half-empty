093d2a9ef7b54bbb5bcb377c1123f3cef1c7ef30
Joe Andaverde <joe@andaverde.net>
2013-06-15 10:45:44 -0500

    Starting point

---
+++ site/index.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+   app = express(),
+   server = require('http').createServer(app),
+   path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', process.env.port || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(require('connect-assets')())
+app.use(require('stylus').middleware(__dirname + '/public'));
+
+require('./controllers/home')(app);
+
+server.listen(app.get('port'));
+console.log('App listening on port ' + app.get('port'));
