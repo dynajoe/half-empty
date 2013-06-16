81ccca0a612c447c08a266220839d3a77fdd6880
Scott Smerchek <scott.smerchek@gmail.com>
2013-06-16 09:09:12 -0500

    Added basic about page.

--- controllers/home.js
+++ controllers/home.js
@@ -2,4 +2,7 @@ module.exports = function (app) {
    app.get('/', function (req, res) {
       res.render('index');
    });
+   app.get('/about', function (req, res) {
+      res.render('about');
+   });
 };
