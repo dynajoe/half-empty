2156436cdb07194503cd988dd6a04f71c23ac60e
Joe Andaverde <joe@andaverde.net>
2013-06-15 19:37:52 -0500

    Wrong env value

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express'),
    path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('port', process.env.port || 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(require('connect-assets')())
