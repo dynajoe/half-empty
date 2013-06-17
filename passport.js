e3f70f3de5896bffd10b9a51c4a175ae373e1d17
Joe Andaverde <joe@andaverde.net>
2013-06-17 17:51:55 -0500

    Hard coded user

--- passport.js
+++ passport.js
@@ -3,17 +3,25 @@ var passport = require('passport')
 
 var TWITTER_CONSUMER_KEY = 'HiRj7aQ8hPXsXYBEW8LMKg';
 var TWITTER_CONSUMER_SECRET = 'a0c9gAQ2gFWP4CkXQrgnAKokrVHZQ6eFCe9dTjM2Pe4';
+var users = {};
 
 passport.use(new TwitterStrategy({
    consumerKey: TWITTER_CONSUMER_KEY,
    consumerSecret: TWITTER_CONSUMER_SECRET,
    callbackURL: "http://half-empty.herokuapp.com/auth/twitter/callback"
 }, function(token, tokenSecret, profile, done) {
-   done(null, { id: 1, token: token, tokenSecret: tokenSecret, profile: profile });
+   var user = { id: 1, token: token, tokenSecret: tokenSecret, profile: profile };
+   users[1] = user.id;
+   done(null, user);
+   console.log(user)
 }));
 
 passport.serializeUser(function(user, done) {
   done(null, user);
+});
+
+passport.deserializeUser(function(id, done) {
+   done(err, users[id]);
 });
 
 module.exports = function (app) {
