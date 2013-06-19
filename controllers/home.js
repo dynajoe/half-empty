d869a2ee46c3adc42df74c98ae413f4d87061899
Scott Smerchek <scott.smerchek@gmail.com>
2013-06-19 12:42:32 -0500

    Add required twitter sign-in for application usage.

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,8 @@
 module.exports = function (app) {
    app.get('/', function (req, res) {
+      if (!req.user) {
+         return res.render('signin');
+      }
       res.render('index');
    });
    app.get('/about', function (req, res) {
