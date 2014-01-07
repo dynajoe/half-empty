258a8f74a74ce97f463268cffa301bb9123ff2a2
Joe Andaverde <joe@andaverde.net>
2014-01-06 22:27:43 -0600

    always use sentiment 140

--- worker/lib/analyzer.js
+++ worker/lib/analyzer.js
@@ -2,12 +2,5 @@ var alchemy = require('./alchemy');
 var sentiment = require('./sentiment');
 
 module.exports.analyze = function (tweets, api, callback) {
-   
-   var analyzer = alchemy;
-
-   if (api === 'sentiment140') {
-      analyzer = sentiment;
-   }
-
-   analyzer.analyzeTweets(tweets, callback);
+   sentiment.analyzeTweets(tweets, callback);
 };
